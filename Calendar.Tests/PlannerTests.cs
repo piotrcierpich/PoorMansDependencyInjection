f4effdaac560c2913ac434fe3121f53720867952
Piotrek <piotr.cierpich@gmail.com>
2013-09-21 11:16:57 +0200

    initial commit

---
+++ Calendar.Tests/PlannerTests.cs
@@ -0,0 +1,19 @@
+﻿using NSubstitute;
+
+using NUnit.Framework;
+
+namespace Calendar.Tests
+{
+    [TestFixture]
+    public class PlannerTests
+    {
+        [Test]
+        public void ShouldListAllEventsWhenAskedFor()
+        {
+            IEventsRepository eventsRepository = Substitute.For<IEventsRepository>();
+            Planner planner = new Planner(eventsRepository);
+            CalendarEvent[] events = planner.GetEvents();
+            eventsRepository.Received(1).GetEvents();
+        }
+    }
+}
