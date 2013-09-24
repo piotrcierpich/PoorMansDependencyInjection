67aa2e9128dfb6c3e2e1fa94e40a5bfd35ad462f
Piotrek <piotr.cierpich@gmail.com>
2013-09-24 11:44:15 +0200

    fixed problem of incorrect input

--- Calendar/UI/DateSpanReader.cs
+++ Calendar/UI/DateSpanReader.cs
@@ -7,48 +7,54 @@ namespace Calendar.UI
 {
     class DateSpanReader
     {
-        private const string DateFormat = "MMdd";
+      private const string DateFormat = "MMdd";
 
-        public static DateSpan PromptForDateSpan()
+      public static DateSpan PromptForDateSpan()
+      {
+        DateSpan dateSpan;
+        while (!TryPromptForDateSpan(out dateSpan))
+        { }
+        return dateSpan;
+      }
+
+      private static bool TryPromptForDateSpan(out DateSpan dateSpan)
+      {
+        try
         {
-            DateSpan dateSpan;
-            while (!TryPromptForDateSpan(out dateSpan))
-            { }
-            return dateSpan;
+          DateTime startDate = ReadDate(string.Format("Start date (format {0}): ", DateFormat));
+          DateTime endDate = ReadDate(string.Format("End date (format {0}):", DateFormat));
+          dateSpan = new DateSpan(startDate, endDate);
+          return true;
+        }
+        catch (ArgumentException argumentException)
+        {
+          Console.WriteLine("Error occured: " + argumentException.Message);
+          dateSpan = DateSpan.Max;
+          return false;
+        }
+      }
+
+      private static DateTime ReadDate(string prompt)
+      {
+        DateTime dateRead;
+        string startDateAsString = PromptAndRead(prompt);
+        while (!DateTime.TryParseExact(startDateAsString,
+                                       DateFormat,
+                                       CultureInfo.InvariantCulture,
+                                       DateTimeStyles.None,
+                                       out dateRead))
+        {
+          Console.WriteLine("Incorrect date, expected format '{0}'", DateFormat);
+          startDateAsString = PromptAndRead(prompt);
         }
 
-        private static bool TryPromptForDateSpan(out DateSpan dateSpan)
-        {
-            try
-            {
-                DateTime startDate = ReadDate(string.Format("Start date (format {0}): ", DateFormat));
-                DateTime endDate = ReadDate(string.Format("End date (format {0}):", DateFormat));
-                dateSpan = new DateSpan(startDate, endDate);
-                return true;
-            }
-            catch (ArgumentException argumentException)
-            {
-                Console.WriteLine("Error occured: " + argumentException.Message);
-                dateSpan = DateSpan.Max;
-                return false;
-            }
-        }
+        return dateRead;
+      }
 
-        private static DateTime ReadDate(string prompt)
-        {
-            DateTime dateRead;
-            Console.Write(prompt);
-            string startDateAsString = Console.ReadLine();
-            while (!DateTime.TryParseExact(startDateAsString,
-                                           DateFormat,
-                                           CultureInfo.InvariantCulture,
-                                           DateTimeStyles.None,
-                                           out dateRead))
-            {
-                Console.WriteLine("Incorrect date, expected format '{0}'", DateFormat);
-            }
-
-            return dateRead;
-        }
+      private static string PromptAndRead(string prompt)
+      {
+        Console.Write(prompt);
+        return Console.ReadLine();
+      }
     }
 }
